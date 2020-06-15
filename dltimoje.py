c7a13d1c9d1dd426345c38849c48dec6e5e649ba
mykyta brazhynskyy <mykyta.brazhynskyy@gmail.com>
2020-06-15 17:58:34 +0200

    d

--- dltimoje.py
+++ dltimoje.py
@@ -5,23 +5,30 @@ import math
 from scipy.optimize import fmin
 from scipy.interpolate import interp1d
 from scipy import signal
-
+import pprint
+#http://home.agh.edu.pl/~pautom/pliki/wyklady/przykladowe/06.pdf
 
 f = open("psk.txt", "r")
 
-
+ilosc=30
 def resp(omega_n,dzeta,k):
-  funkcja=signal.dlti([omega_n*omega_n*k], [1,2*omega_n*dzeta, omega_n*omega_n], dt=0.005)
-  t, y = signal.dimpulse(funkcja, n=61)
-  y=np.squeeze(y)
+  y=[0]*40
+  if dzeta>0:
+    funkcja=signal.dlti([omega_n*omega_n*k], [1,2*omega_n*dzeta, omega_n*omega_n], dt=0.005)
+    t, y = signal.dimpulse(funkcja, n=40)
+    y=np.squeeze(y)
+    
   return y 
 def czytaj():
   t=[]
   y=[]
+  inde=0
   for line in f:
-    kolumny=line.split()
-    t.append(float(kolumny[0]))
-    y.append(float(kolumny[1]))
+    if inde<20:
+      kolumny=line.split()
+      t.append(float(kolumny[0]))
+      y.append(float(kolumny[1]))
+    inde+=1
   return t,y
 t,y=czytaj()
 
@@ -30,33 +37,49 @@ def linearyzuj(t,y):
   nowe_y=[]
   dt=t[1]-t[0]
   for idx,i in enumerate(t):
-    if idx<30:
-        nowe_t.append(i)
-        nowe_t.append((dt/2)+i )
+      nowe_t.append(i)
+      nowe_t.append((dt/2)+i )
   f = interp1d(t, y, fill_value="extrapolate", kind='cubic')
   nowe_y=f(nowe_t)
   return nowe_t,nowe_y
 nowet,nowey=linearyzuj(t,y)
+
 def squares(param):
   sqr = 0
   trans=resp(param[0], param[1], param[2])
   for idx,i in enumerate(nowey):
       sqr += (float(i) -trans[idx] )**2
   wynik=math.sqrt(sqr*(1/len(nowey)))
-  return sqr
+  return wynik
 
 def stworzTeorie(dopasowane):
   teorytyczne=resp(dopasowane[0],dopasowane[1],dopasowane[2])
   return teorytyczne
 def dopasuj():
-  startowa_old=[ 0.01,0.001,6]
+  startowa_old=[ 0.1,0.1,6]
   fit=fmin(squares,startowa_old,maxiter=10000)
   return fit
 
 fitted=dopasuj()
 wynikowe=stworzTeorie(fitted)
+print('omega_n = ',fitted[0],'dzeta = ',fitted[1],'k = ',fitted[2])
 
-# plt.plot(t,y,t,wynikowe,nowet,nowey)
-plt.plot(nowet,nowey,nowet,wynikowe)
 
+plt.plot(nowet,nowey,label='Dane')
+przesuniete_t=[]
+for i in nowet:
+  przesuniete_t.append(i-0.005)
+plt.plot(przesuniete_t,wynikowe,label='Model')
+plt.title('Liczba próbek pomiarowych=20')
+plt.ylabel('G(t) [V/V]')
+plt.xlabel('t [s]')
+plt.legend()
 plt.show()
+
+funkcja=signal.dlti([fitted[0]*fitted[0]*fitted[2]], [1,2*fitted[0]*fitted[1], fitted[0]*fitted[0]], dt=0.005)
+t_n,y_n=signal.dstep(funkcja, n=40)
+y_n=np.squeeze(y_n)
+plt.plot(t_n,y_n)
+plt.show()
+#pprint.pprint(funkcja._as_ss())
+pprint.pprint(funkcja._as_zpk())
---
+++ model.m
@@ -0,0 +1,9 @@
+omega_n=0.84;
+dzeta=8.4e-10;
+k=3.35;
+numerator = [k*omega_n*omega_n];
+denominator = [1,2*dzeta*omega_n,omega_n*omega_n];
+ts = 0.005;
+sys_dyskretny = tf(numerator,denominator,ts);
+step(sys_dyskretny)
+
