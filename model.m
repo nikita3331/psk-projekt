a45264d5ae7006349347c2cb5601acf452671859
mykyta brazhynskyy <mykyta.brazhynskyy@gmail.com>
2020-06-18 10:46:03 +0200

    d

--- model.m
+++ model.m
@@ -5,6 +5,11 @@ numerator = [k*omega_n*omega_n];
 denominator = [1,2*dzeta*omega_n,omega_n*omega_n];
 ts = 0.005;
 sys_dyskretny = tf(numerator,denominator,ts);
-sys_nowy=tf([3.35],[ 4.74 1 0],ts);
-bode(sys_dyskretny)
+sys_ref=tf([1 -2 7],[ 1 0 0.125 -0.4375],ts);
+hold on
+impulse(sys_dyskretny)
+impulse(sys_ref)
+legend('moj','ref')
+hold off
+
 
